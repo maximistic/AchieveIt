1fde10efafc8974b29796aca48e695bd020f13b5
maximistic <110153672+maximistic@users.noreply.github.com>
2025-01-27 08:53:51 +0530

    sidebar

---
+++ app/components/sidebar/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import * as React from 'react'
+
+function page() {
+  return (
+    <div className='bg-[var(--color-bg)] text-[var(--color-text)]'>
+        Sidebar
+    </div>
+  )
+}
+
+export default page
+
+
+/*
+Sidebar icons - dashboard, goals, habits, calendar, analytics, help
+
+Mobile view:
+bottom nav - dashboard, goals, habits, calendar
+*/
