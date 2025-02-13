790ce762016374bad03e4d5f78ca667638b5e0fb
maximistic <110153672+maximistic@users.noreply.github.com>
2025-02-13 10:59:58 +0530

    layout update for nav and side bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased flex min-h-screen">
+      <body className="antialiased min-h-screen bg-[var(--color-bg)] text-[var(--color-text)]">
+        <div className="flex min-h-screen p-4">
+          <Sidebar />
+          <div className="flex-1 flex flex-col md:pl-20">
             <Navbar />
-            <Sidebar />
-            <main className="flex-1 overflow-auto">
+            <main className="flex-1 overflow-auto pt-4">
               {children}
             </main>
+          </div>
+        </div>
       </body>
     </html>
   );
